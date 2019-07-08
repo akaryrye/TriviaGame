f34a7af060dad5cc501b308098187a59dc352809
akaryrye <ralldrin@gmail.com>
2019-07-07 19:17:55 -0700

    linked CSS/JS

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+body = document.getElementById("body")
+newDiv = document.createElement("div");
+
+newDiv.textContent = "Javascript Test: if you can see this message, we are good to go!";
+body.appendChild(newDiv)
